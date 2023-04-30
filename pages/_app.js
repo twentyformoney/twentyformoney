bd804e4bc50828303af53f729be9d08ce1317092
unknown <24mreborn1@gmail.com>
2023-04-30 13:55:30 +0800

    add New Page About

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.scss'
 import { Inter } from '@next/font/google'
+import Navbar from './components/navbar'
 
 const inter = Inter({ subsets: ['latin']})
 export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
+      <Navbar/>
         <Component {...pageProps} />
     </main>
   )
--- pages/components/navbar.jsx
+++ pages/components/navbar.jsx
@@ -1,18 +1,23 @@
+import Link from 'next/link'
 import styles from '/styles/Navbar.module.scss'
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    return (
         <navbar>
-        <div className={styles.Navbar}>
-          <h1>Twentyformoney</h1>
-            <div className={styles.Container_head}>
-              <div className={styles.Containerselect_head}>
-                <span> About</span>
-              </div>
-              <div className={styles.Containerselect_head}>
-                <span> Projects</span>
-              </div>
+            <div className={styles.Navbar}>
+                <Link href='/'><h1>Twentyformoney</h1></Link>
+                <div className={styles.Container_head}>
+                    <Link href='/about'>
+                        <div className={styles.Containerselect_head}>
+                            <span>About</span>
+                        </div>
+                    </Link>
+                    <Link href='/project'>
+                        <div className={styles.Containerselect_head}>
+                            <span>Project</span>
+                        </div>
+                    </Link>
+                </div>
             </div>
-        </div>
         </navbar>
     )
 }
