d48c0355aa0c67fa0493b6a982068db2c94146ed
unknown <24mreborn1@gmail.com>
2023-05-01 10:58:53 +0800

    fix and improve navbar

--- pages/components/navbar.jsx
+++ pages/components/navbar.jsx
@@ -1,21 +1,39 @@
 import Link from 'next/link'
 import styles from '/styles/Navbar.module.scss'
+import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { useRouter } from "next/router";
+import { useState } from 'react';
 export default function Navbar() {
+    const router = useRouter();
+    const [active, setActive] = useState(false);
     return (
         <navbar>
             <div className={styles.Navbar}>
-                <Link href='/'><h1>Twentyformoney</h1></Link>
-                <div className={styles.Container_head}>
-                    <Link href='/about'>
-                        <div className={styles.Containerselect_head}>
-                            <span>About</span>
+                <div className={styles.NavLeft}>
+                    <Link href='/'><h1>Twentyformoney</h1></Link>
+                    <div className={active ? styles.ActiveOption : styles.Option}>
+            
+                        <div className={styles.Container_head}>
+                            <Link href='/about'>
+                                <li className={router.pathname=="/about"?styles.active : ''}>
+                                    <span>About</span>
+                                </li>
+                            </Link>
+                            <Link href='/project'>
+                                <li className={styles.Containerselect_head}>
+                                    <span>Project</span>
+                                </li>
+                            </Link>
                         </div>
-                    </Link>
-                    <Link href='/project'>
-                        <div className={styles.Containerselect_head}>
-                            <span>Project</span>
-                        </div>
-                    </Link>
+                    </div> 
+                </div>
+                <div className={styles.NavRight}>
+                    <div className={styles.BurgerOpsi} onClick={() => setActive(!active)}>
+                        <IconContext.Provider value={{ color: "white", size: "1.5em" }}>
+                            <FaBars />
+                        </IconContext.Provider>
+                    </div>
                 </div>
             </div>
         </navbar>
