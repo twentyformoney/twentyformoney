7c1bb48fd7c9c6e77da63ef6b5b13770107e463a
unknown <24mreborn1@gmail.com>
2023-05-04 21:03:07 +0800

    menambahkan analystic pada app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import '../styles/globals.scss'
 import { Inter } from '@next/font/google'
 import Navbar from './components/navbar'
-
+import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin']})
 export default function App({ Component, pageProps }) {
   return (
+    
     <main className={inter.className}>
+      
       <Navbar/>
         <Component {...pageProps} />
+        <Analytics/>
     </main>
   )
 }
