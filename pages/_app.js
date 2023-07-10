e72bc24466c3ec3d710c9c13df4a0da375177681
unknown <24mreborn1@gmail.com>
2023-07-10 22:34:02 +0800

    first branch

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,18 @@ import '../styles/globals.scss'
 import { Inter } from '@next/font/google'
 import Navbar from './components/navbar'
 import { Analytics } from '@vercel/analytics/react';
+import { useEffect } from 'react';
+import AOS from "aos";
+
+import 'aos/dist/aos.css';
+
 const inter = Inter({ subsets: ['latin']})
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+    });
+  }, []);
   return (
     
     <main className={inter.className}>
